d1b112eb93f8124b8264d38a46f79a54d5736e2a
praveensmart760-beep <praveensmart760@gmail.com>
2026-02-06 12:32:27 +0530

    Add program to check if a number has three divisors in c

---
+++ THREE DIVISORS 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+      int n,count = 0;
+    scanf("%d",&n);
+    for(int i = 1;i<=n;i++){
+        if(n%i == 0){
+            count++;
+        }
+    }
+     if(count == 3){
+         printf("true");
+     }
+    else
+        printf("false");
+    return 0;
+}
