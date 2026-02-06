aaa7e76b7db1bb9d2265a6ea5a94ced60ec2302d
praveensmart760-beep <praveensmart760@gmail.com>
2026-02-06 12:28:25 +0530

    Add Fibonacci sequence calculation program
    Implement Fibonacci sequence calculation for user input.

---
+++ Fibonacci Number 43.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+       int n;
+       scanf("%d",&n);
+        int a = 0;
+        int b = 1;
+        int c = 0;
+      for(int i = 2;i<=n;i++){
+          c = a + b;
+          a = b;
+          b = c;
+      }
+    printf("%d",c);
+return 0;
+}
